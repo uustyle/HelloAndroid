7c1e2f9cabf003b391165d645b1d7c642f8c768f
arakawa <arakawa@yukihiro-VAIO>
2014-02-09 13:17:35 +0900

    a

--- HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.helloandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+
 	}
 
 	@Override
